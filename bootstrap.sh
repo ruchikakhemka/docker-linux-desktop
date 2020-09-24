287d199c5c5ce738d77435ad2cbab8e2c3518ac4
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2020-09-24 13:19:59 +0530

    Add another user

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Starting dbus
 service dbus start
 
-for Username in manali ketan atif prakash kannan nisha ashi;
+for Username in manali ketan atif prakash kannan nisha ashi divya;
 do
   useradd -ms /usr/bin/zsh ${Username}
   usermod -aG sudo ${Username}
@@ -12,6 +12,7 @@ do
   echo "${WindowManager}-session" > /home/${Username}/.xsession
   su - ${Username} sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   su - ${Username} sh -c "cd .oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting && git clone https://github.com/zsh-users/zsh-autosuggestions"
+  su - ${Username} sh -c "rm -rf ~/.asdf; git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0"
 done
 
 /usr/bin/supervisord -n
